9b1d3ea45d9bd02b3a25383b41340886b4126c29
gregroyclark <gregroyclark@gmail.com>
2024-01-12 02:23:31 -0800

    added useParams added loading state & UI fixed returned data updated styles

--- src/components/ViewFriend.jsx
+++ src/components/ViewFriend.jsx
@@ -1,8 +1,14 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ViewFriend = () => {
   const [friend, setFriend] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const { id } = useParams();
+  if (!id) {
+    return;
+  }
 
   useEffect(() => {
     const readOneFriend = async () => {
@@ -11,9 +17,9 @@ const ViewFriend = () => {
         console.log('token: ', token);
         const userId = localStorage.getItem('userId');
         console.log('userId: ', userId);
-
+        console.log('id: ', id);
         const response = await fetch(
-          `http://localhost:5000/api/friend/readOneFriend/${id}`,
+          `https://friendrecord-express.onrender.com/api/friends/readOneFriend/${id}`,
           {
             method: 'GET',
             headers: {
@@ -25,42 +31,49 @@ const ViewFriend = () => {
         );
         const data = await response.json();
         console.log(data);
-        setFriend(data);
+        setFriend(data.rows[0]);
+        setLoading(false);
       } catch (error) {
         console.log('Error reading one friend: ', error);
       }
     };
 
     readOneFriend();
-  });
+  }, [id]);
+
+  if (loading) {
+    return (
+      <div className='flex justify-center items-center m-2 p-2 rounded-sm shadow-md'>
+        Loading...
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <h1>
-        {friend.firstName}
-        {''}
-        {friend.lastName}
+    <div className='m-2 p-2 rounded-sm shadow-md'>
+      <h1 className='flex justify-center items-center text-lg font-semibold text-gray-600'>
+        {friend.firstName} {friend.lastName}
       </h1>
-      <hr />
-      <div>
-        <label>First Name: </label>
-        <p>{friend.firstName}</p>
+      <hr className='mb-4' />
+      <div className='flex p-2'>
+        <label className='m-2'>First Name: </label>
+        <p className='m-2'>{friend.firstName}</p>
       </div>
-      <div>
-        <label>Last Name: </label>
-        <p>{friend.lastName}</p>
+      <div className='flex p-2'>
+        <label className='m-2'>Last Name: </label>
+        <p className='m-2'>{friend.lastName}</p>
       </div>
-      <div>
-        <label>Email: </label>
-        <p>{friend.email}</p>
+      <div className='flex p-2'>
+        <label className='m-2'>Email: </label>
+        <p className='m-2'>{friend.email}</p>
       </div>
-      <div>
-        <label>Phone Number: </label>
-        <p>{friend.phoneNumber}</p>
+      <div className='flex p-2'>
+        <label className='m-2'>Phone Number: </label>
+        <p className='m-2'>{friend.phoneNumber}</p>
       </div>
-      <div>
-        <label>Notes:</label>
-        <p>{friend.notes}</p>
+      <div className='flex p-2'>
+        <label className='m-2'>Notes:</label>
+        <p className='m-2'>{friend.notes}</p>
       </div>
     </div>
   );
