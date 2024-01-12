1eaabfb387c8260f794c5f78abdda84e95548827
gregroyclark <gregroyclark@gmail.com>
2024-01-12 02:42:33 -0800

    fixed route path, ViewFriend

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
           }
         />
         <Route
-          path='/ViewFriend'
+          path='/ViewFriend/:id'
           element={
             <AuthenticatedRoute>
               <ViewFriend />
